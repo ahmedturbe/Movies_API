18cbf8af7ce5819d96d5f8ff85f01537fbbba003
ErvinC <ervin.cajic@gmail.com>
2025-04-17 12:12:44 +0200

    Removed Passport and related migrations migrating to Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,13 +8,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
 
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
